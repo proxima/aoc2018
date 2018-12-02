bfc40153e79cb94468a08aad1b9594cbd8c61061
Christopher Lee <proxima@botchris.localdomain>
2018-12-02 11:20:44 -0600

    Clean up part two.

--- 2/soln.cr
+++ 2/soln.cr
@@ -6,8 +6,8 @@ def part_one
   file.each_line do |l|
     groups = l.each_char.group_by { |c| c }
 
-    num_two   += 1 if groups.find { |k,v| v.size == 2 }
-    num_three += 1 if groups.find { |k,v| v.size == 3 }
+    num_two   += 1 if groups.any? { |k,v| v.size == 2 }
+    num_three += 1 if groups.any? { |k,v| v.size == 3 }
   end
 
   num_two * num_three
@@ -15,28 +15,18 @@ end
 
 def part_two
   file = File.open "input.txt", "r"
-  
+
   file.each_line.to_a.permutations(2).each do |perm|
-    next if perm[0].size != perm[1].size
+    zipped = perm.first.chars.zip(perm.last.chars)
 
-    prefix = 0
-    until perm[0][prefix] != perm[1][prefix]
-      prefix += 1
-    end
-
-    perm[0] = perm[0].reverse
-    perm[1] = perm[1].reverse
-
-    suffix = 0
-    until perm[0][suffix] != perm[1][suffix]
-      suffix += 1
-    end
-
-    perm[0] = perm[0].reverse
-    perm[1] = perm[1].reverse
-
-    if prefix + suffix == perm[0].size - 1
-      return "#{perm[0][0, prefix]}#{perm[0][prefix + 1, suffix]}"
+    common = zipped.select { |a, b| a == b }
+    
+    if common.size == perm.first.size - 1
+      zipped.each_with_index do |combined, index|
+        if combined.first != combined.last
+          return "#{perm.first[0, index]}#{perm.first[index + 1, perm.first.size - index]}"
+        end
+      end
     end
   end
 end
