468b8a0ac96495c2b212590fa8eddec73d72b945
Christopher Lee <proxima@botchris.localdomain>
2018-12-03 08:35:53 -0600

    Crystal Day 3

---
+++ 3/soln.cr
@@ -0,0 +1,42 @@
+require "set"
+
+def solution
+  file = File.open "input.txt", "r"
+  
+  locations = {} of Tuple(Int32, Int32) => Array(Int32)
+  overlaps  = {} of Int32 => Set(Int32)
+  
+  file.each_line do |l|
+    match = l.match /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/
+
+    p match
+
+    match.try do |m|
+      claim, x, y, w, h = m[1].to_i, m[2].to_i, m[3].to_i, m[4].to_i, m[5].to_i
+
+      overlaps[claim] = Set(Int32).new
+
+      (x...x+w).each do |row|
+        (y...y+h).each do |col|
+          if !locations.includes?({row, col})
+            locations[{row, col}] = [] of Int32
+          else
+            locations[{row, col}].each do |c|
+              overlaps[claim].add c
+              overlaps[c].add claim
+            end
+          end
+
+          locations[{row, col}] << claim
+        end
+      end
+    end
+  end
+
+  p locations.values.select { |x| x.size > 1 }.size
+  overlaps.find { |k, v| v.size == 0 }.try do |overlap|
+    p overlap.first
+  end
+end
+
+solution
